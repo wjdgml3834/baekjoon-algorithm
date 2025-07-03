872a57b0d69f6c735a188e2f2c9f10d811bf00bf
Junghee Kim <88475978+wjdgml3834@users.noreply.github.com>
2025-07-04 00:38:30 +0900

    [Bronze II] Title: 상수, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2908. 상수/상수.py
+++ 백준/Bronze/2908. 상수/상수.py
@@ -1,19 +1,16 @@
-lst = list(input().split())
-A = lst[0]
-B = lst[1]
-A_reverse = ""
-B_reverse = ""
+lst = input().split()
+f = lst[0]
+r = lst[1]
+fnum = 0
+rnum = 0
+sq = 1
 
-for i in range(len(A)-1,-1,-1):
-    A_reverse = A_reverse + A[i]
+for i in range(0,3,1):
+    fnum = fnum + int(f[i]) * sq
+    rnum = rnum + int(r[i]) * sq
+    sq = sq * 10
 
-for i in range(len(B)-1,-1,-1):
-    B_reverse = B_reverse + B[i]
-
-A_reverse = int(A_reverse)
-B_reverse = int(B_reverse)
-
-if A_reverse > B_reverse:
-    print(A_reverse)
+if fnum > rnum:
+    print(fnum)
 else:
-    print(B_reverse)
+    print(rnum)
