1b2df050f819dd176a945d36a2b8fee5172c031d
Junghee Kim <88475978+wjdgml3834@users.noreply.github.com>
2025-07-08 09:43:56 +0900

    [Bronze II] Title: 나머지, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/3052. 나머지/나머지.py
@@ -0,0 +1,16 @@
+lst = []
+result = []
+
+for i in range(0,10,1):
+    n = int(input())
+    remain = n % 42
+    lst.append(remain)
+
+for i in range(0,len(lst),1):
+    if lst.count(lst[i]) >= 2:
+        if lst[i] not in result:
+            result.append(lst[i])
+    elif lst.count(lst[i]) == 1:
+        result.append(lst[i])
+                        
+print(len(result))
