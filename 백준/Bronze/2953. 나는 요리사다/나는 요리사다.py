6fb8180a629ab224b46c7cd7a9ad3a7ef2ba4918
Junghee Kim <88475978+wjdgml3834@users.noreply.github.com>
2025-07-15 08:37:34 +0900

    [Bronze III] Title: 나는 요리사다, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2953. 나는 요리사다/나는 요리사다.py
+++ 백준/Bronze/2953. 나는 요리사다/나는 요리사다.py
@@ -1,16 +1,13 @@
-total_sum = 0
 mx = 0 
-p = 0
+mxidx = 0
 
 for i in range(0,5,1):
     lst = list(map(int,input().split()))
-    total_sum = 0
-    
-    for j in range(0,4,1):
-        total_sum += lst[j]
-        
-    if mx < total_sum:
-        mx = total_sum
-        p = i+1
-        
-print(p, mx)
+    total = 0 
+    for j in lst: 
+        total += j
+    if mx < total:
+        mx = total
+        mxidx = i+1 
+
+print(mxidx, mx)
