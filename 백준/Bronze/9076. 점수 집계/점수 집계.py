c802814f7713a73d2d8925c98765b938a3aba9f2
Junghee Kim <88475978+wjdgml3834@users.noreply.github.com>
2025-07-16 17:12:05 +0900

    [Bronze II] Title: 점수 집계, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/9076. 점수 집계/점수 집계.py
+++ 백준/Bronze/9076. 점수 집계/점수 집계.py
@@ -2,14 +2,17 @@ n = int(input())
 
 for i in range(0,n,1):
     lst = list(map(int, input().split()))
-    mx = max(lst)
-    mn = min(lst)
+    lst.sort()
+    mx = lst[len(lst) - 1]
+    mn = lst[0]
     lst.remove(mx)
     lst.remove(mn)
-    mx = max(lst)
-    mn = min(lst)
+    mx = lst[len(lst) - 1]
+    mn = lst[0]
     if mx - mn >= 4:
         print("KIN")
     else:
-        total = sum(lst)
-        print(total)
+        temp_total = 0
+        for j in lst:
+            temp_total += j
+        print(temp_total)
