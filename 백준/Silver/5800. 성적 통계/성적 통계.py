56f2593ba644ab338a6a4dcb45c61a61b2e7a61d
Junghee Kim <88475978+wjdgml3834@users.noreply.github.com>
2025-07-21 00:25:26 +0900

    [Silver V] Title: 성적 통계, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/5800. 성적 통계/성적 통계.py
@@ -0,0 +1,17 @@
+n = int(input())
+
+for i in range(0,n,1):
+    mx = -1
+    mn = 101 
+    gapmx = -1
+    lst = list(map(int,input().split()))
+    lst.remove(lst[0])
+    lst.sort(reverse=True)
+    mx = lst[0]
+    mn = lst[len(lst)-1]
+    for j in range(0,len(lst)-1,1):
+        diff = lst[j] - lst[j+1]
+        if diff > gapmx:
+            gapmx = diff
+    print(f"Class {i+1}")
+    print(f"Max {mx}, Min {mn}, Largest gap {gapmx}")
