9ffbccbbc05ab99e3590d18a1cc878207b878c61
Junghee Kim <88475978+wjdgml3834@users.noreply.github.com>
2025-07-22 00:05:16 +0900

    [Silver V] Title: 소트인사이드, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.py
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.py
@@ -1,18 +1,7 @@
-lst = []
-desc_lst = []
-inp = input()
-for i in inp:
-    i = int(i)
-    lst.append(i)
 
-lst.sort(reverse=True)
-
+n = input()
+lst = list(n)
+lst.sort()
+lst.reverse()
 for i in lst:
-    i = str(i)
-    desc_lst.append(i)
-
-st = "".join(desc_lst)
-
-n = int(st)
-
-print(n)
+    print(i, end='')
