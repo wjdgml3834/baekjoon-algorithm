529dc69d038765f10724160a98a5935f95727687
Junghee Kim <88475978+wjdgml3834@users.noreply.github.com>
2025-08-05 21:30:45 +0900

    [Bronze II] Title: 알파벳 거리, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/5218. 알파벳 거리/알파벳 거리.py
+++ 백준/Bronze/5218. 알파벳 거리/알파벳 거리.py
@@ -1,21 +1,14 @@
 n = int(input())
 
-for i in range(0,n,1):
-    distance = []
-    lst = input().split()
-    f = lst[0]
-    r = lst[1]
-    for j in range(0,len(f),1):
-        x = f[j]
-        y = r[j]
-        if y >= x:
-            result = ord(y) - ord(x)
-            distance.append(result)
-        else:
-            result = (ord(y) + 26) - ord(x)
-            distance.append(result)
-          
-    for k in range(n):
-        str_value = [str(x) for x in distance]
-        joined = " ".join(str_value)
-    print("Distances: " + joined)
+for i in range(n):
+	st = input().split()
+	st1 = st[0]
+	st2 = st[1]
+	print("Distances:", end=" ")
+	for idx in range(0,len(st1),1):
+		gap = ord(st1[idx]) - ord(st2[idx])
+		if gap > 0 :
+			gap = 26 - gap
+		print(abs(gap), end= " ")
+		
+	print() # 줄바꿈
