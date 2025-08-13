289106d5b3b3ce0195421287ec472aa3f9f098d1
Junghee Kim <88475978+wjdgml3834@users.noreply.github.com>
2025-08-14 01:42:11 +0900

    [Bronze II] Title: 알파벳 전부 쓰기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/11091. 알파벳 전부 쓰기/알파벳 전부 쓰기.py
+++ 백준/Bronze/11091. 알파벳 전부 쓰기/알파벳 전부 쓰기.py
@@ -1,25 +1,28 @@
-n = int(input())
-alphabet = "abcdefghijklmnopqrstuvwxyz"
+N = int(input())
 
-for _ in range(n):
+for _ in range(N):
     lst = [0] * 26
-    sentence = input().lower()
-    stripped_sentence = sentence.replace(" ", "")
-    for ch in stripped_sentence:
-        idx = alphabet.find(ch)
-        if idx != -1:
-            lst[idx] += 1
+    st = input().lower()
+    st2 = ""
+    for ch in st:
+        gap = ord(ch) - ord("a")
+        if gap >=0 and gap <= 25:
+            st2 = st2 + ch
+    
+    for ch in st2:
+        idx = ord(ch) - ord("a")
+        lst[idx] += 1
     
     is_pangram = True
     missing_words = ""
     
     for idx in range(0,len(lst),1):
         if lst[idx] == 0:
-            missing_st = chr(idx + ord("a"))
-            missing_words = missing_words + missing_st
+            missing_ch = chr(idx + ord("a"))
+            missing_words += missing_ch
             is_pangram = False
     
     if is_pangram:
         print("pangram")
-    else: 
+    else:
         print(f"missing {missing_words}")
