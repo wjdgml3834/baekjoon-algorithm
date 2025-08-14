d96a642277f4dc983bd08ec6f8dde8711c252121
Junghee Kim <88475978+wjdgml3834@users.noreply.github.com>
2025-08-15 02:06:54 +0900

    [Bronze I] Title: 단어 퍼즐, Time: 1756 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/9946. 단어 퍼즐/단어 퍼즐.py
+++ 백준/Bronze/9946. 단어 퍼즐/단어 퍼즐.py
@@ -1,12 +1,12 @@
-n = 1
+n = 1 # 케이스 횟수를 세기 위함
 while True:
-    st1 = input()
-    st2 = input()
+    st1 = input() # 처음 완성한 알파벳들
+    st2 = input() # 회수한 알파벳들
     
-    if st1 == 'END':
+    if st1 == 'END': # END가 나오면 바로 while문 탈출
         break
     
-    lst1 = [0] * 26
+    lst1 = [0] * 26 # 알바벳 담을 리스트
     lst2 = [0] * 26
     
     for i in range(0,len(st1),1):
@@ -19,19 +19,26 @@ while True:
         idx = ord(ch) - ord("a")
         lst2[idx] +=1
         
-    is_same = "same"
+    is_same = True
     
     if len(lst1) >= len(lst2):
         for i in range(0,len(lst1),1):
             if lst1[i] != lst2[i]:
-                is_same = "different"
+                is_same = False
                 break
     else:
         for i in range(0,len(lst2),1):
             if lst1[i] != lst2[i]:
-                is_same = "different"
+                is_same = False
                 break
     
-    print(f"Case {n}: {is_same}")
+    result = ""
     
-    n += 1
+    if is_same:
+        result = "same"
+    else:
+        result = "different"
+    
+    print(f"Case {n}: {result}")
+    
+    n += 1 # 케이스 횟수 올림
