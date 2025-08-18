60ac3df617997d1c0f517424dbd5bee9c8a8635c
Junghee Kim <88475978+wjdgml3834@users.noreply.github.com>
2025-08-19 03:20:21 +0900

    [Bronze I] Title: 이항 계수 1, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/11050. 이항 계수 1/이항 계수 1.py
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.py
@@ -4,12 +4,9 @@ K = lst[1]
 c = 1
 p = 1
 
-for i in range(N, N-K, -1):
+for i in range(N,N-K,-1):
     c = c * i
+    p = p * (N - (i-1))
 
-for i in range(K,0,-1):
-    p = p * i
-    
 result = c // p
-
 print(result)
