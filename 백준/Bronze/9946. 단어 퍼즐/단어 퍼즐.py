39944a01f14021046e545caf70430ec612ce1c2a
Junghee Kim <88475978+wjdgml3834@users.noreply.github.com>
2025-08-26 01:07:07 +0900

    [Bronze I] Title: 단어 퍼즐, Time: 1004 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/9946. 단어 퍼즐/단어 퍼즐.py
+++ 백준/Bronze/9946. 단어 퍼즐/단어 퍼즐.py
@@ -1,44 +1,26 @@
-n = 1 # 케이스 횟수를 세기 위함
+n = 1
 while True:
-    st1 = input() # 처음 완성한 알파벳들
-    st2 = input() # 회수한 알파벳들
+    st1 = input()
+    st2 = input()
     
-    if st1 == 'END': # END가 나오면 바로 while문 탈출
+    if st1 == "END":
         break
     
-    lst1 = [0] * 26 # 알바벳 담을 리스트
-    lst2 = [0] * 26
+    lst1 = list(st1)
+    lst2 = list(st2)
     
-    for i in range(0,len(st1),1):
-        ch = st1[i]
-        idx = ord(ch) - ord("a")
-        lst1[idx] +=1
+    lst1.sort()
+    lst2.sort()
     
-    for i in range(0,len(st2),1):
-        ch = st2[i]
-        idx = ord(ch) - ord("a")
-        lst2[idx] +=1
+    is_same = False
+    
+    if lst1 == lst2:
+        is_same = True
+    
+    if is_same == True:
+        print("Case %d: same" %n)
         
-    is_same = True
-    
-    if len(lst1) >= len(lst2):
-        for i in range(0,len(lst1),1):
-            if lst1[i] != lst2[i]:
-                is_same = False
-                break
     else:
-        for i in range(0,len(lst2),1):
-            if lst1[i] != lst2[i]:
-                is_same = False
-                break
-    
-    result = ""
-    
-    if is_same:
-        result = "same"
-    else:
-        result = "different"
-    
-    print(f"Case {n}: {result}")
-    
-    n += 1 # 케이스 횟수 올림
+        print("Case %d: different" %n)
+        
+    n += 1
