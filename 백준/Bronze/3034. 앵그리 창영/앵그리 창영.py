85a9227049db5f41ed555799e33f6a6c2357fb2b
Junghee Kim <88475978+wjdgml3834@users.noreply.github.com>
2025-08-25 11:14:58 +0900

    [Bronze III] Title: 앵그리 창영, Time: 36 ms, Memory: 34536 KB -BaekjoonHub

--- 백준/Bronze/3034. 앵그리 창영/앵그리 창영.py
+++ 백준/Bronze/3034. 앵그리 창영/앵그리 창영.py
@@ -1,14 +1,14 @@
 import math
 
 lst= list(map(int,input().split()))
-N = lst[0]
-W = lst[1]
-H = lst[2]
+N = lst[0] # 성냥의 개수
+W = lst[1] # 성냥박스 가로길이
+H = lst[2] # 성냥박스 세로길이 
 
 for _ in range(N):
     length = int(input())
     
-    mx = math.sqrt(W ** 2 + H **2)
+    mx = math.sqrt(W ** 2 + H **2) # 성냥박스가 들어갈 수 있는 최대 = 대각선 길이
     
     if length <= mx:
         print("DA")
