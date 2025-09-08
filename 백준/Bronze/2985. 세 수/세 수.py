5fe78062751a8f7f79e3851928bd9de48df138d7
Junghee Kim <88475978+wjdgml3834@users.noreply.github.com>
2025-09-09 07:19:20 +0900

    [Bronze III] Title: 세 수, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2985. 세 수/세 수.py
+++ 백준/Bronze/2985. 세 수/세 수.py
@@ -1,28 +1,35 @@
-import operator
-
 lst = list(map(int,input().split()))
 a = lst[0]
 b = lst[1]
 c = lst[2]
-ops = {
-    "+" : operator.add,
-    "-" : operator.sub,
-    "*" : operator.mul,
-    "/" : operator.truediv
-}
-flag = False
 
-while True:
-    if flag == True:
-        break
+if a + b == c:
+    print(f"{a}+{b}={c}")
+
+
+elif a - b == c:
+    print(f"{a}-{b}={c}")
     
-    for x in ops.keys():
-        if ops[x](a,b) == c:
-            print(f"{a}{x}{b}={c}")
-            flag = True
-            break
-        
-        elif ops[x](b,c) == a:
-             print(f"{a}={b}{x}{c}")
-             flag = True
-             break
+
+elif a * b == c:
+    print(f"{a}*{b}={c}")
+
+
+elif a / b == c:
+    print(f"{a}/{b}={c}")
+    
+    
+elif b + c == a:
+    print(f"{a}={b}+{c}")
+
+
+elif b - c == a:
+    print(f"{a}={b}-{c}")
+
+
+elif b * c == a:
+    print(f"{a}={b}*{c}")
+
+
+elif b / c == a:
+    print(f"{a}={b}/{c}")
