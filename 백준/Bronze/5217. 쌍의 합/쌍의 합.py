50795d388c172860ecbedf7600b5a873b17ebd6a
Junghee Kim <88475978+wjdgml3834@users.noreply.github.com>
2025-09-08 11:45:48 +0900

    [Bronze III] Title: 쌍의 합, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/5217. 쌍의 합/쌍의 합.py
+++ 백준/Bronze/5217. 쌍의 합/쌍의 합.py
@@ -1,20 +1,17 @@
 T = int(input())
 
-for i in range(0, T, 1):
-    m = int(input())
+for _ in range(T):
+    n = int(input())
+    cnt = 0 
+    print("Pairs for %d:" %n, end=" ")
     
-    print("Pairs for %d:" %m, end=" ")
-    
-    cnt = 0
-    
-    for j in range(1,13,1):
-        for k in range(j+1,13,1):
-            hap = j + k
-            
-            if hap ==m:
-                if cnt >0:
-                    print(", ", end="")
-                print(j,k,end="")
+    for i in range(1,13,1):
+        for j in range(i+1,13,1):
+            total = i + j 
+            if total == n:
+                if cnt > 0:
+                    print(",", end=" ")
                 
-                cnt +=1 
+                print(i,j,end="")
+                cnt += 1 
     print()
