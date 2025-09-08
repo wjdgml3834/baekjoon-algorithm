7e23ae377c538334b71962c646c54419e9e20592
Junghee Kim <88475978+wjdgml3834@users.noreply.github.com>
2025-09-09 05:53:17 +0900

    [Silver IV] Title: 주몽, Time: 44 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/1940. 주몽/주몽.py
+++ 백준/Silver/1940. 주몽/주몽.py
@@ -1,29 +1,27 @@
-n = int(input())
-m = int(input())
-
+N = int(input())
+M = int(input())
 lst = list(map(int,input().split()))
 
-lst.sort() # 필수
+lst.sort() # 우선 오름차순으로 정렬하는 것이 중요
 
-left = 0
-right = n-1
-
-ans = 0 
+left = 0  # 왼쪽 포인터
+right = N - 1 # 오른쪽 포인터
+rst = 0 # 결과 담을 변수
 
 while True:
-    if left == right:
+    if left == right: # 두 포인터가 같아지면 break
         break
     
     total = lst[left] + lst[right]
     
-    if total == m:
-        ans += 1
+    if total == M: # 같으면 rst 하나 올리고, 오른쪽 포인터를 내린다.
+        rst += 1
         right -= 1
         
-    elif total > m:
+    elif total > M: # total이 크면 오른쪽 포인터를 내린다.
         right -= 1
         
-    else:
-        left += 1
-
-print(ans)
+    else: # 그 외에는 왼쪽 포인터를 올린다.
+        left += 1 
+    
+print(rst)
