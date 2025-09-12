05e1895732402e4d17049ec0f072d294c6306d63
Junghee Kim <88475978+wjdgml3834@users.noreply.github.com>
2025-09-13 01:30:43 +0900

    [Bronze II] Title: 단어의 개수, Time: 40 ms, Memory: 34368 KB -BaekjoonHub

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.py
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.py
@@ -1,3 +1,14 @@
-st = input().split()
-rst = len(st)
+st = input()
+
+cnt = st.count(" ")
+
+if st[0] == " ":
+    cnt -= 1
+
+if st[len(st)-1] == " ":
+    cnt -= 1
+
+rst = cnt + 1
+
 print(rst)
+    
