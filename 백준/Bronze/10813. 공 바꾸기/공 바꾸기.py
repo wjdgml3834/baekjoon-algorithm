9959e78d877d677c3960721e420f19af249ed2c8
Junghee Kim <88475978+wjdgml3834@users.noreply.github.com>
2025-09-29 02:16:59 +0900

    [Bronze II] Title: 공 바꾸기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10813. 공 바꾸기/공 바꾸기.py
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.py
@@ -1,23 +1,23 @@
 N, M = map(int,input().split())
 lst = []
 
-for i in range(1,N+1,1):
+for i in range(1,N+1,1): # 우선 바구니 리스트를 만들어준다. 
     lst.append(i)
     
 for _ in range(M):
-    idx1, idx2 = map(int,input().split())
-    if idx1 == idx2:
+    idx1, idx2 = map(int,input().split()) 
+    if idx1 == idx2: # 바구니 번호가 같다면 아무 변화도 일으키지 않는다.
         continue
-    idx1 = idx1 - 1 
+    idx1 = idx1 - 1 # 0부터 시작하기 위해 -1 해준다. 
     idx2 = idx2 - 1
     
-    ball1 = lst.pop(idx1) 
+    ball1 = lst.pop(idx1) # 공을 1개 빼서
     
-    lst.insert(idx2, ball1)
+    lst.insert(idx2, ball1) # idx2번째 인덱스에 넣는다.
     
-    ball2 = lst.pop(idx2-1)
+    ball2 = lst.pop(idx2-1) # 빼줄때는 리스트가 변화되어서 -1 해주어야 한다.  
     
-    lst.insert(idx1, ball2)
+    lst.insert(idx1, ball2) # idx1번째 인덱스에 넣는다.
 
     
 for i in lst:
