d966bc8d762ac30ded9c03849496092a042c82e0
Junghee Kim <88475978+wjdgml3834@users.noreply.github.com>
2025-10-05 01:32:25 +0900

    [Bronze I] Title: 희주의 수학시험, Time: 76 ms, Memory: 36480 KB -BaekjoonHub

--- 백준/Bronze/8974. 희주의 수학시험/희주의 수학시험.py
+++ 백준/Bronze/8974. 희주의 수학시험/희주의 수학시험.py
@@ -1,16 +1,15 @@
-lst = []
+lst = [] # 수열을 만들 빈 리스트
 
-for i in range(1,1001,1):
+for i in range(1,1001,1): # 수열을 담아준다. 
     for _ in range(i):
         lst.append(i)
 
 
 A,B = map(int,input().split())
-total = 0 
+total = 0 # 총합을 구할 변수 
 
-
-for i in range(A,B+1,1):   
-    num = int(lst[i-1])
+for i in range(A,B+1,1): 
+    num = int(lst[i-1]) # 인덱스는 -1 해야 한다. 
     total += num
 
 print(total)
