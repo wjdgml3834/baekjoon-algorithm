2f0e23ecb5c5dc30539e869ebd6348c3774da58f
Junghee Kim <88475978+wjdgml3834@users.noreply.github.com>
2025-10-06 01:32:09 +0900

    [Bronze II] Title: 숫자 카드놀이, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/4564. 숫자 카드놀이/숫자 카드놀이.py
+++ 백준/Bronze/4564. 숫자 카드놀이/숫자 카드놀이.py
@@ -1,7 +1,7 @@
-def func(n):
-    total = 1
-    while n > 0:
-        r = n % 10
+def func(n): # 자릿수를 곱하는 함수 
+    total = 1 
+    while n > 0: # 자릿수를 하나씩 분해해서 곱하는 로직 
+        r = n % 10 
         total *= r
         n = n // 10
     
@@ -10,13 +10,13 @@ def func(n):
 while True:
     S = int(input())
     
-    if S == 0:
+    if S == 0: # 0을 입력받으면 반복문을 종료한다. 
         break
     
-    lst = []
-    lst.append(S)
+    lst = [] # 결과를 담을 리스트 
+    lst.append(S) # 첫 값은 무조건 넣어준다.
     
-    while S >= 10:
+    while S >= 10: # 한자릿수 될때까지 계속 함수 호출해서 반복한다. 
         new_num = func(S)
         S = new_num
         lst.append(S)
@@ -24,4 +24,4 @@ while True:
     for i in lst:
         print(i, end=" ")
 
-    print()
+    print() # 입력 편하게 하기 위함 
