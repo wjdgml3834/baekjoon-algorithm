d061dd429276526d935c200fd03100cd5736ad66
Junghee Kim <88475978+wjdgml3834@users.noreply.github.com>
2025-10-14 02:36:40 +0900

    [Bronze III] Title: 공, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/1547. 공/공.py
+++ 백준/Bronze/1547. 공/공.py
@@ -1,14 +1,14 @@
-M = int(input())
-lst = [1,2,3]
+M = int(input()) # 위치를 바꾸는 횟수
+lst = [1,2,3] # 컵 3개 리스트 
 
 for _ in range(M):
     x,y = map(int,input().split())
-    idx1 = lst.index(x)
+    idx1 = lst.index(x) # x,y 값의 인덱스를 찾아준다.
     idx2 = lst.index(y)
     
-    tmp = lst[idx1]
+    tmp = lst[idx1] # 찾은 값들을 서로 바꾼다. 
     lst[idx1] = lst[idx2]
     lst[idx2] = tmp
 
-rst = lst[0]
+rst = lst[0] # 1번공이 들어있는 즉 가장 첫번째 요소를 출력한다. 
 print(rst)
